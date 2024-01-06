dbc0ded91e51e86dfeb36502473fbcfbe506c1d0
Bang NGUYEN <lelybang.nguyen@gmail.com>
2024-01-06 23:56:21 +0100

    Terminal catppuccin

---
+++ .config/alacritty/catppuccin.sh
@@ -0,0 +1,8 @@
+# mocha
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
+# macchiato
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-macchiato.toml
+# frappe
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-frappe.toml
+# latte
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-latte.toml
