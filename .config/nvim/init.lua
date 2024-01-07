5c35eee34d68cdf0a4dd0bd2e54bd9707585b68b
Bang NGUYEN <lelybang.nguyen@gmail.com>
2024-01-07 01:15:09 +0100

    Basic keymap & settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,19 +4,44 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 0
+vim.o.expandtab = false
+vim.o.incsearch = true
+
+-- Clear search highlight
+vim.keymap.set('n', "<C-c>", ":nohls<CR>")
+
+-- Relative line numbers toggle
+vim.o.cursorline = true
+vim.o.number = true
+vim.o.relativenumber = true
+
+_G.toggle_line_numbers = function()
+	if vim.o.relativenumber then
+		vim.o.number = true
+		vim.o.relativenumber = false
+	else
+		vim.o.number = true
+		vim.o.relativenumber = true
+	end
+end
+vim.keymap.set('n', "<CR>", "<cmd>lua toggle_line_numbers()<CR>", { noremap = true, silent = true})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
